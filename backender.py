2871b6c9d1ad5225d7489122ba9a003b4febd12f
noahamac <noah.macdonald@looker.com>
2019-10-25 10:50:22 -0700

    category_label logic

--- backender.py
+++ backender.py
@@ -3,7 +3,7 @@ import datetime
 import sys
 
 with open(sys.argv[1], "r") as read_file:
-    decoded = json.load(read_file)
+	decoded = json.load(read_file)
 
 label = decoded['label']
 category_label = decoded['category_label']
@@ -11,6 +11,13 @@ image = decoded['branding']['image_uri']
 tagline = decoded['branding']['tagline']
 
 now = datetime.datetime.now() 
+
+if category_label == 'Models':
+	category_label = 'tools'
+elif category_label == 'Plug-ins':
+	category_label= 'visualizations'
+elif category_label == 'Applications':
+  	category_label = 'applications'
 
 listing = """
 import {{ MarketplaceListing, fileURL }} from "../../src/data"
@@ -21,36 +28,36 @@ const listing: MarketplaceListing = {{
   label: {label},
   author: byLooker,
   branding: {{
-    image_uri: [ fileURL("block-icons/FILENAME.png") ],
-    tagline:
-      "{tagline}",
+	image_uri: [ fileURL("block-icons/FILENAME.png") ],
+	tagline:
+	  "{tagline}",
   }},
   versions: [
-    {{
-      number: "1.0.0",
-      looker_version: ">=6.20.1",
-      released_at: "{now}",
-      release_notes_md: "Initial Marketplace Release",
-      project_git_uri: "[ GITHUB REPO ]",
-      project_git_ref: "[ GITHUB COMMIT REF ]",
-    }},
+	{{
+	  number: "1.0.0",
+	  looker_version: ">=6.20.1",
+	  released_at: "{now}",
+	  release_notes_md: "Initial Marketplace Release",
+	  project_git_uri: "[ GITHUB REPO ]",
+	  project_git_ref: "[ GITHUB COMMIT REF ]",
+	}},
   ],
   connection_hub_details: connectionHubDetails("Zendesk data"),
   hero_image_uris: [
-    "[ IMAGE 1 ]",
-    "[ IMAGE 2 ]",
+	"[ IMAGE 1 ]",
+	"[ IMAGE 2 ]",
   ],
   metadata_fields: [
-    {{
-      id: "etl-providers",
-      label: "ETL Providers",
-      values: ["Looker", "Fivetran"],
-    }},
-    {{
-      id: "dialects",
-      label: "SQL Dialects",
-      values: ["Google BigQuery"],
-    }},
+	{{
+	  id: "etl-providers",
+	  label: "ETL Providers",
+	  values: ["Looker", "Fivetran"],
+	}},
+	{{
+	  id: "dialects",
+	  label: "SQL Dialects",
+	  values: ["Google BigQuery"],
+	}},
   ],
   documentation_uri: "https://docs.looker.com/apps/coolzone",
   support_uri: "https://docs.looker.com/support/coolzone",
@@ -62,7 +69,7 @@ export default listing
 """.format(label=label, tagline=tagline, now=now, category_label=category_label.lower())
 
 with open("listing.ts", "w") as write_file:
-    write_file.write(listing)
+	write_file.write(listing)
 
 
 
